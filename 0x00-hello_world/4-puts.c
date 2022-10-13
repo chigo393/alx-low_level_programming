9d5dccc508108af20791295896801ec18e698204
chigo393 <chijiokepatrick393@gmail.com>
2022-10-13 15:14:13 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle.\n");
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
