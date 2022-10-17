20e630781a459e6448ed2a5b364ff237b687f15a
chigo393 <chijiokepatrick393@gmail.com>
2022-10-17 03:30:50 -0700

    print alphabets in lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print alphabet in lower case
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc ='a'; lc <= 'z'; lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
