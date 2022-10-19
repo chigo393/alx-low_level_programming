08eef9c774c04cb3014d497636a77a30f2879c31
chigo393 <chijiokepatrick393@gmail.com>
2022-10-19 15:49:35 -0700

    print putchar with new linw

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - printing _putchar with a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	Return (0);
+}
